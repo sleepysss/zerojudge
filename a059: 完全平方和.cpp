01e1da51083c8c0065a4cdfd262423834664e144
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-22 19:58:17 +0800

    Create a059: 完全平方和.cpp

---
+++ a059: 完全平方和.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int T,a,b,total,l,r,mid;
+    
+    cin>>T;
+    
+    for(int i=0;i<T;++i)
+    {
+        total=0;
+        cin>>a>>b;
+        
+        for(int j=a;j<=b;++j)
+        {
+            //用binary search看j是否為完全平方數
+            l=0;
+            r=j;
+            while(l<=r)
+            {
+                
+                mid=l+(r-l)/2;
+                if(mid*mid>j)
+                    r=mid-1;
+                else if(mid*mid<j)
+                    l=mid+1;
+                else
+                {
+                    total+=j;
+                    break;
+                }
+            
+            }
+        }
+        
+        cout<<"Case "<<i+1<<": "<<total<<endl;
+    }
+    
+    return 0;
+}
