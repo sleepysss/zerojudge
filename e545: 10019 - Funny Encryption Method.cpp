1a28474739f8dc125fa4df878e6c44d62faec67e
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-23 12:32:29 +0800

    Create e545: 10019 - Funny Encryption Method.cpp

---
+++ e545: 10019 - Funny Encryption Method.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int power16(int a)
+{
+    int sum=1;
+    for(int i=0;i<a;++i)
+        sum*=16;
+    return sum;
+}
+
+int main()
+{
+    vector<bool> v1,v2;
+    int count,num,x,sum,count2,b1,b2;
+    cin>>count;
+    
+    for(int i=0;i<count;++i)
+    {
+        cin>>num;
+        v1.clear();
+        v2.clear();
+        b1=0;
+        b2=0;
+        x=num;
+        //10轉2
+        while(x)
+        {
+            v1.push_back(x%2);
+            x/=2;
+        }
+        //b1有幾個1
+        for(int i=0;i<v1.size();++i)
+        {
+            if(v1[i]==1)
+                b1++;
+        }
+        
+        x=num;
+        count2=0;
+        sum=0;
+        //16轉10
+        while(x)
+        {
+            sum=sum+(x%10)*power16(count2);
+            x/=10;
+            count2++;
+        }
+         
+        while(sum)
+        {
+            v2.push_back(sum%2);
+            sum/=2;
+        }
+        //b2有幾個1
+        for(int i=0;i<v2.size();++i)
+        {
+            if(v2[i]==1)
+                b2++;
+        }
+        
+        cout<<b1<<" "<<b2<<endl;
+    }
+    
+    return 0;
+}
