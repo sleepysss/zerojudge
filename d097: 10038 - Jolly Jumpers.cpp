af992d44f38d8bce9715184f4bf6e8518098e70f
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-23 21:32:35 +0800

    Update d097: 10038 - Jolly Jumpers.cpp

--- d097: 10038 - Jolly Jumpers.cpp
+++ d097: 10038 - Jolly Jumpers.cpp
@@ -1,3 +1,5 @@
+//method 1 : 讀整行
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -40,6 +42,65 @@ int main()
             v2.push_back(diff);
         }
         
+        num=1;
+        sort(v2.begin(),v2.end());
+        for(int i=0;i<v2.size();++i)
+        {
+            if(v2[i]!=num)
+            {
+                flag=0;
+                break;
+            }
+            num++;
+        }
+        
+        if(flag)
+            cout<<"Jolly"<<endl;
+        else
+            cout<<"Not jolly"<<endl;
+    }
+    
+    return 0;
+}
+
+//method 2 :讀數字
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v1,v2;
+    bool flag;
+    int num,diff,count;
+    while(cin>>count)
+    {
+        v1.clear();
+        v2.clear();
+        flag=1;
+        
+        for(int i=0;i<count;++i)
+        {
+            cin>>num;
+            v1.push_back(num);
+        }
+        
+        //計算各數之間的差
+        for(int i=0;i<v1.size()-1;++i)
+        {
+            diff=abs(v1[i]-v1[i+1]);
+            if(diff>(count-1))
+            {
+                flag=0;
+                break;
+            }
+            v2.push_back(diff);
+        }
+        
+        //看是否每個都有
         num=1;
         sort(v2.begin(),v2.end());
         for(int i=0;i<v2.size();++i)
